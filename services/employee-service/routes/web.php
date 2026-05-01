d1ecb79eac938c7c2d4ac1557913766678370cdc
Hesham-prog <alsamihesham@gmail.com>
2026-05-02 00:53:43 +0700

    fix(employee): memindahkan route ke api.php agar response berbentuk JSON

--- services/employee-service/routes/web.php
+++ services/employee-service/routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::prefix('api')->group(function () {
-    Route::get('/employees', [EmployeeController::class, 'index']);
-    Route::post('/employees', [EmployeeController::class, 'store']);
-    Route::get('/employees/{employee}', [EmployeeController::class, 'show']);
-    Route::put('/employees/{employee}', [EmployeeController::class, 'update']);
-    Route::delete('/employees/{employee}', [EmployeeController::class, 'destroy']);
 });
